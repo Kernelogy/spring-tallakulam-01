834ffcde076681248d367b041abb434f4c971e50
Kernelogy <kernelogy@gmail.com>
2025-04-09 18:44:56 +0530

    Sending data to server

--- src/main/java/com/edex/commerce/controller/ProductController.java
+++ src/main/java/com/edex/commerce/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.edex.commerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,13 @@ public class ProductController {
                 .header("edex-header", "Welcome to edex")
                 .body(product);       
     }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id){
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Cotton Shirt", "Pure Cotton Shirt", 599.99, "shirt1.jpg"));
+        products.add(new Product("Linen Shirt", "Pure Linen Shirt", 599.99, "shirt2.jpg"));
+        products.add(new Product("Polyster Shirt", "Polyster Shirt", 599.99, "shirt3.jpg"));
+        return ResponseEntity.ok().body(products.get(id));
+    }
+
 }
--- src/main/java/com/edex/commerce/controller/UserController.java
+++ src/main/java/com/edex/commerce/controller/UserController.java
@@ -2,8 +2,13 @@ package com.edex.commerce.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edex.dto.request.Login;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -12,10 +17,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
     @GetMapping("/login")
     public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
+        /*
         System.out.println("Username : " + username);
         System.out.println("Password : " + password);
         return ResponseEntity.ok()
         .body("Success");
+        */
+        if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("12345")){
+            return ResponseEntity.ok()
+            .body("Success");
+        }else{
+            return ResponseEntity.ok()
+            .body("Failed");
+        }
     }
-    
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Login user){
+        String username = user.getUsername();
+        String password = user.getPassword();
+        if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("12345")){
+            return ResponseEntity.ok()
+            .body("Success");
+        }else{
+            return ResponseEntity.ok()
+            .body("Failed");
+        }       
+
+    }
+
 }
